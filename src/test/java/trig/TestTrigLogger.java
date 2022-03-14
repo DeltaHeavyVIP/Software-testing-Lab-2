db82f3a0e9b204f9317969ce615adf2cff519e5d
DeltaHeavyVIP <patutin.vovan@mail.ru>
2022-03-14 12:53:39 +0300

    Logger

---
+++ src/test/java/trig/TestTrigLogger.java
@@ -0,0 +1,83 @@
+package trig;
+
+import com.opencsv.CSVReader;
+import lab_2.trig.*;
+import lab_2.utils.CsvLogger;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.csv.Csv;
+
+import static java.lang.Math.PI;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("Trig logger")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class TestTrigLogger {
+
+    private final Sin sin;
+    private final Cos cos;
+    private final Tan tan;
+    private final Cot cot;
+    private final Sec sec;
+    private final Csc csc;
+    private final TrigCalculator trigCalculator;
+    private static CsvLogger csvLogger;
+    private final double accuracy = 0.1;
+
+    TestTrigLogger() {
+        this.sin = new Sin();
+        this.cos = new Cos();
+        this.tan = new Tan();
+        this.cot = new Cot();
+        this.sec = new Sec();
+        this.csc = new Csc();
+        this.trigCalculator = new TrigCalculator(sin, cos, tan, cot, sec, csc);
+
+    }
+
+    @Test
+    @DisplayName("Logger sin(x) test")
+    void sinTest() {
+        csvLogger = new CsvLogger("sin.csv", -6 * PI, -3 * PI, 0.5 * PI);
+        csvLogger.logger(sin);
+    }
+
+    @Test
+    @DisplayName("Logger cos(x) test")
+    void cosTest() {
+        csvLogger = new CsvLogger("cos.csv", -6 * PI, -3 * PI, 0.5 * PI);
+        csvLogger.logger(cos);
+    }
+
+    @Test
+    @DisplayName("Logger tan(x) test")
+    void tanTest() {
+        csvLogger = new CsvLogger("tan.csv", -6 * PI, -3 * PI, 0.5 * PI);
+        csvLogger.logger(sin);
+    }
+
+
+    @Test
+    @DisplayName("Logger cot(x) test")
+    void cotTest() {
+        csvLogger = new CsvLogger("cot.csv", -6 * PI, -3 * PI, 0.5 * PI);
+        csvLogger.logger(sin);
+    }
+
+    @Test
+    @DisplayName("Logger sec(x) test")
+    void secTest() {
+        csvLogger = new CsvLogger("sec.csv", -6 * PI, -3 * PI, 0.5 * PI);
+        csvLogger.logger(sin);
+    }
+
+    @Test
+    @DisplayName("Logger csc(x) test")
+    void cscTest() {
+        csvLogger = new CsvLogger("csc.csv", -6 * PI, -3 * PI, 0.5 * PI);
+        csvLogger.logger(sin);
+    }
+}
--- src/test/java/trig/trigStubsTest.java
+++ src/test/java/trig/trigStubsTest.java
@@ -1,54 +1,54 @@
-package trig;
-
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvException;
-import lab_2.trig.*;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
-
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-public class trigStubsTest {
-
-    private static Sin sin = mock(Sin.class);
-    private static Cos cos = mock(Cos.class);
-    private static Tan tan = mock(Tan.class);
-    private static Cot cot = mock(Cot.class);
-    private static Sec sec = mock(Sec.class);
-    private static Csc csc = mock(Csc.class);
-    private static Calculator calculator = null;
-
-    @BeforeAll
-    public static void setup() throws IOException, CsvException {
-        fillMock(sin, "/inputTrig/sinData.csv");
-        fillMock(cos, "/inputTrig/cosData.csv");
-        fillMock(tan, "/inputTrig/tanData.csv");
-        fillMock(cot, "/inputTrig/cotData.csv");
-        fillMock(sec, "/inputTrig/secData.csv");
-        fillMock(csc, "/inputTrig/cscData.csv");
-        calculator = new Calculator(sin, cos, tan, cot, sec, csc);
-    }
-
-    private static void fillMock(TrigFunction tf, String tableName) throws IOException, CsvException {
-        try (CSVReader csvReader = new CSVReader(new FileReader(tableName))) {
-            List<String[]> records = csvReader.readAll();
-            for (String[] record : records) {
-                final double x = Double.parseDouble(record[0]);
-                final double y = Double.parseDouble(record[1]);
-                when(tf.apply(x)).thenReturn(y);
-            }
-        } catch (IOException | CsvException exp) {
-            throw exp;
-        }
-    }
-
-}
+//package trig;
+//
+//import com.opencsv.CSVReader;
+//import com.opencsv.exceptions.CsvException;
+//import lab_2.trig.*;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.TestInstance;
+//import org.junit.jupiter.params.ParameterizedTest;
+//import org.junit.jupiter.params.provider.CsvFileSource;
+//
+//import java.io.FileReader;
+//import java.io.IOException;
+//import java.util.List;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.mockito.Mockito.mock;
+//import static org.mockito.Mockito.when;
+//
+//public class trigStubsTest {
+//
+//    private static Sin sin = mock(Sin.class);
+//    private static Cos cos = mock(Cos.class);
+//    private static Tan tan = mock(Tan.class);
+//    private static Cot cot = mock(Cot.class);
+//    private static Sec sec = mock(Sec.class);
+//    private static Csc csc = mock(Csc.class);
+//    private static TrigCalculator trigCalculator = null;
+//
+//    @BeforeAll
+//    public static void setup() throws IOException, CsvException {
+//        fillMock(sin, "/inputTrig/sinData.csv");
+//        fillMock(cos, "/inputTrig/cosData.csv");
+//        fillMock(tan, "/inputTrig/tanData.csv");
+//        fillMock(cot, "/inputTrig/cotData.csv");
+//        fillMock(sec, "/inputTrig/secData.csv");
+//        fillMock(csc, "/inputTrig/cscData.csv");
+//        trigCalculator = new TrigCalculator(sin, cos, tan, cot, sec, csc);
+//    }
+//
+//    private static void fillMock(TrigFunction tf, String tableName) throws IOException, CsvException {
+//        try (CSVReader csvReader = new CSVReader(new FileReader(tableName))) {
+//            List<String[]> records = csvReader.readAll();
+//            for (String[] record : records) {
+//                final double x = Double.parseDouble(record[0]);
+//                final double y = Double.parseDouble(record[1]);
+//                when(tf.apply(x)).thenReturn(y);
+//            }
+//        } catch (IOException | CsvException exp) {
+//            throw exp;
+//        }
+//    }
+//
+//}
